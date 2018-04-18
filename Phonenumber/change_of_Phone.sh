6d294febf08d99872a29ad4e59f1cc1f77c082ca
Vasdev Gullapalli <vasdevg@qualcomm.com>
2018-04-17 18:36:22 -0600

    update find command with option name
    Only html files need update

--- Phonenumber/change_of_Phone.sh
+++ Phonenumber/change_of_Phone.sh
@@ -2,4 +2,4 @@
 
 #The tool is to find and replace all the phone numbers to a static value
 
-find /var/www/ -type f -exec sed -ri 's/.[0-9]+[^0-9]*[a-zA-Z0-9]+[^0-9]*[a-zA-Z0-9]+[-. ]*[a-zA-Z0-9]+./ 202-456-1414 /g' {} +
+find /var/www/ -type f -name "*html" -exec sed -ri 's/.[0-9]+[^0-9]*[a-zA-Z0-9]+[^0-9]*[a-zA-Z0-9]+[-. ]*[a-zA-Z0-9]+./ 202-456-1414 /g' {} +
